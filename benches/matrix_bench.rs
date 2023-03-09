5e49d7d91529a3ce4465c000ee31f74cc82e828e
Joey Li <joey@joeyli.dev>
2023-03-09 22:14:49 +0100

    numpy

--- benches/matrix_bench.rs
+++ benches/matrix_bench.rs
@@ -9,6 +9,9 @@ use linearalgebra::matrix::Matrix;
 use rand::Rng;
 use test::Bencher;
 
+use numpy::PyArray2;
+use pyo3::{types::IntoPyDict, Python};
+
 fn setup(len: usize) -> (Vec<f64>, Vec<f64>) {
     let mut rng = rand::thread_rng();
 
@@ -20,42 +23,65 @@ fn setup(len: usize) -> (Vec<f64>, Vec<f64>) {
 
 #[bench]
 fn matrix_simple_multiplication_bench(b: &mut Bencher) {
-
-    let (m1,m2) = setup(3000 * 32);
+    let (m1, m2) = setup(3000 * 32);
 
     let matrix1 = Matrix::new(3000, 32, m1).unwrap();
     let matrix2 = Matrix::new(32, 3000, m2).unwrap();
 
     b.iter(|| {
-        matrix1.simple_product_matrix(&matrix2);
+        matrix1.simple_product_matrix(&matrix2).unwrap();
     })
 }
 
 #[bench]
 fn matrix_trivial_multiplication_bench(b: &mut Bencher) {
-
-    let (m1,m2) = setup(3000 * 32);
+    let (m1, m2) = setup(3000 * 32);
 
     let matrix1 = Matrix::new(3000, 32, m1).unwrap();
     let matrix2 = Matrix::new(32, 3000, m2).unwrap();
 
     b.iter(|| {
-        matrix1.trivial_product_matrix(&matrix2);
+        matrix1.trivial_product_matrix(&matrix2).unwrap();
     })
 }
 
+#[bench]
+fn big_matrix_multiplication_numpy_test(b: &mut Bencher) {
+    let (m1, m2) = setup(3000 * 32);
+
+    b.iter(|| {
+        Python::with_gil(|py| {
+            let np = py.import("numpy").unwrap();
+            let locals = [("np", np)].into_py_dict(py);
+            locals.set_item("m1", &m1).unwrap();
+            locals.set_item("m2", &m2).unwrap();
+
+            let result: &PyArray2<f64> = py
+                .eval(
+                    "np.matmul(np.reshape(m1, (32,3000)), np.reshape(m2, (3000,32)))",
+                    Some(locals),
+                    None,
+                )
+                .unwrap()
+                .extract()
+                .unwrap();
+
+            Matrix::new(32, 32, result.to_vec().unwrap()).unwrap();
+        })
+    })
+}
 
 #[test]
 fn big_matrix_multiplication_test() {
     let pre_random = Instant::now();
-    let (m1,m2) = setup(3000 * 32);
+    let (m1, m2) = setup(3000 * 32);
     println!("{}", pre_random.elapsed().as_millis());
 
     let multiplication = Instant::now();
     let matrix1 = Matrix::new(3000, 32, m1).unwrap();
     let matrix2 = Matrix::new(32, 3000, m2).unwrap();
 
-    matrix1.product_matrix(&matrix2);
+    matrix1.product_matrix(&matrix2).unwrap();
 
     println!("{}", multiplication.elapsed().as_millis())
 }
